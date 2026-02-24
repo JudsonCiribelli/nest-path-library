dd7f060734ed2f20810d72943d393cda6d8f123d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-24 18:42:28 -0300

    feat: add create author dto

---
+++ src/author/dto/create-author.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  bio?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  birthDate: Date;
+}
