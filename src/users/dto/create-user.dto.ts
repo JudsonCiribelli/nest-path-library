4d69ff910b9080597505f08274c50730b9387896
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-02-25 18:56:48 -0300

    feat: add create user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsMobilePhone,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  @IsMobilePhone('pt-BR')
+  phone?: string;
+}
