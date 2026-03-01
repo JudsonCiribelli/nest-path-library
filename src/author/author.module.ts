b70d38fb208bcbe2bed5f40ed30cbb0b7db8aa23
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-01 11:53:42 -0300

    feat: add exception filters to author module

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { ApiExceptionFilters } from 'src/common/filters/exception-filters';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AuthorService],
   controllers: [AuthorController],
+  providers: [
+    AuthorService,
+    { provide: APP_FILTER, useClass: ApiExceptionFilters },
+  ],
 })
 export class AuthorModule {}
