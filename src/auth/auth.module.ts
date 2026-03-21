fcc24246bd1d77a31c0f65b4825122727a22cda7
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-20 21:30:17 -0300

    refactor: export jwt module and config module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -23,6 +23,6 @@ import { JwtModule } from '@nestjs/jwt';
       useClass: BcryptServiceProtocol,
     },
   ],
-  exports: [HashingServiceProtocol],
+  exports: [HashingServiceProtocol, JwtModule, ConfigModule],
 })
 export class AuthModule {}
