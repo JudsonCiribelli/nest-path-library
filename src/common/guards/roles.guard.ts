f366db2b14fdedfc2ded1ed551f009dcdf64075f
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-23 19:39:50 -0300

    feat: add roles decorator and goles guard

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorator/roles.decorator';
+import { REQUEST_TOKEN_PAYLOAD } from 'src/auth/common/auth.constants';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request[REQUEST_TOKEN_PAYLOAD];
+
+    const hasRole = requiredRoles.some((role) => user?.role === role);
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        'Você não tem permissão para acessar este recurso.',
+      );
+    }
+
+    return true;
+  }
+}
