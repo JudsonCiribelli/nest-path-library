ac9c2dc391602e466e675391ff983ac51699660a
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-24 19:59:47 -0300

    feat: add create author review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { AuthAdminGuard } from 'src/common/guards/admin.guard';
-import { CreateReviewDto } from './dto/create-review.dto';
+import { CreateBookReviewDto } from './dto/create-book-review.dto';
 import { GetUser } from 'src/common/decorator/get-user.decorator';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { CreateAuthorReviewDto } from './dto/create-author-review.dto';
+import { create } from 'domain';
 
 @Controller('reviews')
 export class ReviewController {
@@ -14,13 +25,32 @@ export class ReviewController {
     return this.reviewService.getReviews();
   }
 
+  @Get('/book/:bookId')
+  async getReviewsByBook(@Param('bookId') bookId: string) {
+    return this.reviewService.getBookReviews(bookId);
+  }
+
   @Post('/book/:bookId')
   @UseGuards(AuthAdminGuard)
   async createReview(
-    @Body() createReviewDto: CreateReviewDto,
+    @Body() createReviewDto: CreateBookReviewDto,
     @Param('bookId') bookId: string,
     @GetUser('sub') userId: string,
   ) {
-    return this.reviewService.createReview(userId, createReviewDto, bookId);
+    return this.reviewService.createBookReview(userId, createReviewDto, bookId);
+  }
+
+  @Post('/author/:authorId')
+  @UseGuards(AuthAdminGuard)
+  async createAuthorReview(
+    @Body() createAuthorReviewDto: CreateAuthorReviewDto,
+    @Param('authorId') authorId: string,
+    @GetUser('sub') userId: string,
+  ) {
+    return this.reviewService.createAuthorReview(
+      userId,
+      createAuthorReviewDto,
+      authorId,
+    );
   }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,14 +1,33 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateReviewDto } from './dto/create-review.dto';
+import { CreateBookReviewDto } from './dto/create-book-review.dto';
+import { CreateAuthorReviewDto } from './dto/create-author-review.dto';
 
 @Injectable()
 export class ReviewService {
   constructor(private prisma: PrismaService) {}
 
-  async createReview(
+  async getReviews() {
+    const reviews = await this.prisma.review.findMany();
+
+    if (reviews.length === 0) {
+      throw new HttpException(
+        'Erro ao buscar avaliações!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return reviews;
+  }
+
+  async createBookReview(
     userId: string,
-    createReviewDto: CreateReviewDto,
+    createReviewDto: CreateBookReviewDto,
     bookId: string,
   ) {
     const bookExists = await this.prisma.book.findUnique({
@@ -24,20 +43,68 @@ export class ReviewService {
       throw new NotFoundException('Livro não encontrado!');
     }
 
-    const review = await this.prisma.review.create({
-      data: {
-        userId,
-        stars: createReviewDto.stars,
-        bookId: bookId,
-        comment: createReviewDto.comment,
-        authorId: bookExists.author.id,
+    try {
+      return await this.prisma.review.create({
+        data: {
+          userId,
+          stars: createReviewDto.stars,
+          bookId: bookId,
+          comment: createReviewDto.comment,
+          authorId: bookExists.author.id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Erro ao criar avaliação!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async getBookReviews(bookId: string) {
+    return await this.prisma.book.findUnique({
+      where: {
+        id: bookId,
+      },
+
+      select: {
+        reviews: true,
+      },
+    });
+  }
+
+  async createAuthorReview(
+    userId: string,
+    createAuthorReviewDto: CreateAuthorReviewDto,
+    authorId: string,
+  ) {
+    const authorExists = await this.prisma.author.findUnique({
+      where: {
+        id: authorId,
       },
     });
 
-    return review;
-  }
+    if (!authorExists) {
+      throw new NotFoundException('Autor não encontrado!');
+    }
 
-  async getReviews() {
-    return this.prisma.review.findMany();
+    try {
+      return await this.prisma.review.create({
+        data: {
+          userId,
+          authorId: authorExists.id,
+          bookId: null,
+          stars: createAuthorReviewDto.stars!,
+          comment: createAuthorReviewDto.comment,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        'Erro ao criar avaliação!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
