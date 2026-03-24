8cc39c36d2bb508dda33649486ce11e76d5936f8
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-24 17:33:21 -0300

    refactor: ensure user be logged to create a new loan

--- src/loan/loan.controller.ts
+++ src/loan/loan.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { LoanService } from './loan.service';
 import { CreateLoanDto } from './dto/create-loan';
+import { AuthAdminGuard } from 'src/common/guards/admin.guard';
 
 @Controller('loan')
 export class LoanController {
   constructor(private readonly loanService: LoanService) {}
 
   @Post()
+  @UseGuards(AuthAdminGuard)
   async createLoan(@Body() createLoanDto: CreateLoanDto) {
     return this.loanService.createLoan(createLoanDto);
   }
