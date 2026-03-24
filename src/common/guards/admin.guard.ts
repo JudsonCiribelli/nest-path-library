b06c4d48035411941f4f2374e4ac30063235fcc3
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-24 17:57:03 -0300

    fix(auth): implement JWT validation in AuthAdminGuard and populate request payload

--- src/common/guards/admin.guard.ts
+++ src/common/guards/admin.guard.ts
@@ -1,23 +1,50 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import type { ConfigType } from '@nestjs/config';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { REQUEST_TOKEN_PAYLOAD } from 'src/auth/common/auth.constants';
 
 @Injectable()
 export class AuthAdminGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log('Começando guard');
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
 
-    const request = context
-      .switchToHttp()
-      .getRequest<Request & { users?: { token: string } }>();
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
 
-    console.log(request.users);
+    if (!token) {
+      throw new UnauthorizedException('Token não encontrado!');
+    }
 
-    if (request.users?.token) return true;
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.jwtConfiguration.secret,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      });
 
-    console.log('Finalizando guard');
+      request[REQUEST_TOKEN_PAYLOAD] = payload;
 
-    return false;
+      return true;
+    } catch (error) {
+      console.error('Erro na validação do token:', error.message);
+      throw new UnauthorizedException('Acesso não autorizado!');
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
