f7b6b0f5baa5cb3edc2a0a987eb345353b176192
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-24 16:52:45 -0300

    feat: ensure just user with admin role can create a new category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryService } from './category.service';
 import { AuthAdminGuard } from 'src/common/guards/admin.guard';
+import { Roles } from 'src/common/decorator/roles.decorator';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Controller('category')
 @UseGuards(AuthAdminGuard)
@@ -9,16 +11,13 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
   @Get()
   async findAllCategory() {
-    const categories = await this.categoryService.findAllCategory();
-
-    return categories;
+    return await this.categoryService.findAllCategory();
   }
 
   @Post()
+  @Roles('ADMIN')
+  @UseGuards(AuthAdminGuard, RolesGuard)
   async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
-    const category =
-      await this.categoryService.createCategory(createCategoryDto);
-
-    return category;
+    return await this.categoryService.createCategory(createCategoryDto);
   }
 }
