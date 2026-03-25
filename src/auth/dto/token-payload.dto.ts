947db16e702d66e6e3843d04b504a714ed6400b5
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-25 10:55:19 -0300

    feat: add custom param to prevent users from accessing the accounts of other users who are not logged in

---
+++ src/auth/dto/token-payload.dto.ts
@@ -0,0 +1,18 @@
+import { IsInt, IsString } from 'class-validator';
+
+export class TokenPayloadDto {
+  @IsString()
+  sub: string;
+  @IsString()
+  email: string;
+  @IsString()
+  role: string;
+  @IsInt()
+  iat: number;
+  @IsInt()
+  exp: number;
+  @IsString()
+  aud: string;
+  @IsString()
+  iss: string;
+}
