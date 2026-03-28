2179e0ef3b6163d086f77bbb67bef6df85db264b
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-27 21:57:39 -0300

    feat: add book factorie to make a new book

---
+++ test/factories/book-factory.ts
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker';
+import { CreateBookDto } from 'src/books/dto/create-book.dto';
+
+const author = {
+  id: faker.string.uuid(),
+};
+const category = {
+  id: faker.string.uuid(),
+};
+
+export function makeCreateBookDto(
+  override?: Partial<CreateBookDto>,
+): CreateBookDto {
+  return {
+    title: faker.person.fullName(),
+    description: faker.person.bio(),
+    authorId: author.id,
+    categoryId: category.id,
+    pages: faker.number.int(),
+    year: faker.number.int(),
+    ...override,
+  };
+}
+
+export function makeBookEntity(override?: any) {
+  return {
+    id: faker.string.uuid(),
+    title: faker.person.fullName(),
+    description: faker.person.bio(),
+    authorId: author.id,
+    categoryId: category.id,
+    pages: faker.number.int(),
+    year: faker.number.int(),
+    status: 'AVAILABLE',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...override,
+  };
+}
