5854b5e8b70ef976abb17f558fc401d90b00aff2
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-30 18:55:57 -0300

    feat: add  swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('The Curated Archive')
+    .setDescription('Sistema de reserva de livros.')
+    .addBearerAuth()
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
